760deb11a53320382da22c4ea9fb623d933b62c1
aes3xs <aes3xs@gmail.com>
2017-02-15 23:43:05 +0200

    Add description and phpdoc to files

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -1,12 +1,27 @@
 <?php
 
+/*
+ * This file is part of the Yodler package.
+ *
+ * (c) aes3xs <aes3xs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Aes3xs\Yodler\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Input\InputOption;
 
+/**
+ * Application.
+ */
 class Application extends BaseApplication
 {
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         parent::__construct();
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Yodler package.
+ *
+ * (c) aes3xs <aes3xs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Aes3xs\Yodler;
 
 use Aes3xs\Yodler\Exception\RuntimeException;
@@ -8,13 +17,16 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
+/**
+ * Kernel.
+ */
 class Kernel
 {
     protected $configFile;
     protected $container;
 
     /**
-     * Kernel constructor.
+     * Constructor.
      * @param $configFile
      */
     public function __construct($configFile)
@@ -34,6 +46,9 @@ class Kernel
         return $this->container;
     }
 
+    /**
+     * Initializes container.
+     */
     public function boot()
     {
         $this->container = $this->buildContainer($this->configFile);
