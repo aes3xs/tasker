bc52faf164d405ba86a586ee61f23ff95e0a362e
aes3xs <aes3xs@gmail.com>
2017-02-20 12:26:58 +0200

    Add Scenario

---
+++ src/Scenario/Scenario.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the Yodler package.
+ *
+ * (c) aes3xs <aes3xs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aes3xs\Yodler\Scenario;
+
+use Aes3xs\Yodler\Action\ActionListInterface;
+use Aes3xs\Yodler\Variable\VariableListInterface;
+use Aes3xs\Yodler\Variable\VariableSuppliedInterface;
+
+/**
+ * Scenario implementation.
+ */
+class Scenario implements ScenarioInterface, VariableSuppliedInterface
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var ActionListInterface
+     */
+    protected $actions;
+
+    /**
+     * @var ActionListInterface
+     */
+    protected $failbackActions;
+
+    /**
+     * @var VariableListInterface
+     */
+    protected $variables;
+
+    /**
+     * Constructor.
+     *
+     * @param $name
+     * @param ActionListInterface $actions
+     * @param ActionListInterface $failbackActions
+     * @param VariableListInterface $variables
+     */
+    public function __construct(
+        $name,
+        ActionListInterface $actions,
+        ActionListInterface $failbackActions,
+        VariableListInterface $variables
+    ) {
+        $this->name = $name;
+        $this->actions = $actions;
+        $this->failbackActions = $failbackActions;
+        $this->variables = $variables;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getActions()
+    {
+        return $this->actions;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFailbackActions()
+    {
+        return $this->failbackActions;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVariables()
+    {
+        return $this->variables;
+    }
+}
