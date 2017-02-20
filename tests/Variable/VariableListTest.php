ab0cd53245feaa5f806599888f6cdeb9a4a8060a
aes3xs <aes3xs@gmail.com>
2017-02-20 09:17:53 +0200

    Refactor VariableList test to use Variable mocks instead of concrete classes

--- tests/Variable/VariableListTest.php
+++ tests/Variable/VariableListTest.php
@@ -13,7 +13,6 @@ namespace Aes3xs\Yodler\Tests\Variable;
 
 use Aes3xs\Yodler\Exception\VariableAlreadyExistsException;
 use Aes3xs\Yodler\Exception\VariableNotFoundException;
-use Aes3xs\Yodler\Variable\Variable;
 use Aes3xs\Yodler\Variable\VariableInterface;
 use Aes3xs\Yodler\Variable\VariableList;
 
@@ -21,21 +20,37 @@ class VariableListTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstruct()
     {
-        $variable = new Variable('test', 'value');
+        $variable = $this->createMock(VariableInterface::class);
+        $variable->method('getName')->willReturn('test');
 
         $list = new VariableList([$variable]);
 
-        $this->assertInstanceOf(VariableInterface::class, $list->get('test'));
-        $this->assertEquals('value', $list->get('test')->getValue());
+        $this->assertSame($variable, $list->get('test'));
     }
 
-    public function testAdd()
+    public function testAddAndGet()
     {
         $list = new VariableList();
-        $list->add(new Variable('test', 'value'));
+        $variable = $this->createMock(VariableInterface::class);
+        $variable->method('getName')->willReturn('test');
+        $list->add($variable);
 
-        $this->assertInstanceOf(VariableInterface::class, $list->get('test'));
-        $this->assertEquals('value', $list->get('test')->getValue());
+        $this->assertSame($variable, $list->get('test'));
+    }
+
+    public function testAll()
+    {
+        $list = new VariableList();
+        $variable1 = $this->createMock(VariableInterface::class);
+        $variable1->method('getName')->willReturn('test1');
+        $variable2 = $this->createMock(VariableInterface::class);
+        $variable2->method('getName')->willReturn('test2');
+        $list->add($variable1);
+        $list->add($variable2);
+
+        $this->assertCount(2, $list->all());
+        $this->assertSame($variable1, $list->all()['test1']);
+        $this->assertSame($variable2, $list->all()['test2']);
     }
 
     public function testHas()
@@ -44,7 +59,9 @@ class VariableListTest extends \PHPUnit_Framework_TestCase
 
         $this->assertFalse($list->has('test'));
 
-        $list->add(new Variable('test', 'value'));
+        $variable = $this->createMock(VariableInterface::class);
+        $variable->method('getName')->willReturn('test');
+        $list->add($variable);
 
         $this->assertTrue($list->has('test'));
     }
@@ -63,8 +80,12 @@ class VariableListTest extends \PHPUnit_Framework_TestCase
         $this->expectException(VariableAlreadyExistsException::class);
 
         $list = new VariableList();
+        $variable1 = $this->createMock(VariableInterface::class);
+        $variable1->method('getName')->willReturn('test');
+        $variable2 = $this->createMock(VariableInterface::class);
+        $variable2->method('getName')->willReturn('test');
 
-        $list->add(new Variable('test', 'value'));
-        $list->add(new Variable('test', 'value'));
+        $list->add($variable1);
+        $list->add($variable2);
     }
 }
