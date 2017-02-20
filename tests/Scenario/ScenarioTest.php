0e8925a0cdcb485235a4e3e2e57c583e38c1aa54
aes3xs <aes3xs@gmail.com>
2017-02-20 13:18:47 +0200

    Add Scenario test

---
+++ tests/Scenario/ScenarioTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Yodler package.
+ *
+ * (c) aes3xs <aes3xs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aes3xs\Yodler\Tests\Connection;
+
+use Aes3xs\Yodler\Action\ActionListInterface;
+use Aes3xs\Yodler\Scenario\Scenario;
+use Aes3xs\Yodler\Variable\VariableListInterface;
+
+class ScenarioTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $actionsMock = $this->createMock(ActionListInterface::class);
+        $fallbackActionsMock = $this->createMock(ActionListInterface::class);
+        $variablesMock = $this->createMock(VariableListInterface::class);
+
+        $scenario = new Scenario('test', $actionsMock, $fallbackActionsMock, $variablesMock);
+
+        $this->assertEquals('test', $scenario->getName());
+        $this->assertSame($actionsMock, $scenario->getActions());
+        $this->assertSame($fallbackActionsMock, $scenario->getFailbackActions());
+        $this->assertSame($variablesMock, $scenario->getVariables());
+    }
+}
