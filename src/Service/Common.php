3c7c186644d998373fe2cc315af417f4c96a9509
aes3xs <aes3xs@gmail.com>
2017-03-02 16:37:15 +0200

    Add Common service

---
+++ src/Service/Common.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Yodler package.
+ *
+ * (c) aes3xs <aes3xs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aes3xs\Yodler\Recipe;
+
+use Aes3xs\Yodler\Service\Shell;
+
+/**
+ * Helper service to to some common tasks.
+ */
+class Common
+{
+    /**
+     * @var Shell
+     */
+    protected $shell;
+
+    /**
+     * Constructor.
+     *
+     * @param Shell $shell
+     */
+    public function __construct(Shell $shell)
+    {
+        $this->shell = $shell;
+    }
+
+    /**
+     * @param $basePath
+     * @param array $removePaths
+     * @param array $copyPaths
+     * @param array $createPaths
+     * @param $writablePaths
+     * @param array $readablePaths
+     */
+    public function structurize(
+        $basePath,
+        array $removePaths = [],
+        array $copyPaths = [],
+        array $createPaths = [],
+        array $writablePaths = [],
+        array $readablePaths = []
+    ) {
+        foreach ($removePaths as $path) {
+            $this->shell->rm("$basePath/$path");
+        }
+
+        foreach ($createPaths as $path) {
+            $this->shell->mkdir("$basePath/$path");
+        }
+
+        foreach ($copyPaths as $source => $target) {
+            $this->shell->copy("$basePath/$source", "$basePath/$target");
+        }
+
+        foreach ($writablePaths as $path) {
+            if (!$this->shell->isWritable("$basePath/$path")) {
+                throw new \RuntimeException('Path not writable: ' . "$basePath/$path");
+            }
+        }
+
+        foreach ($readablePaths as $path) {
+            if (!$this->shell->isReadable("$basePath/$path")) {
+                throw new \RuntimeException('Path not readable: ' . "$basePath/$path");
+            }
+        }
+    }
+}
