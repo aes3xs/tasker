c40cf890c67c62ce4c0481058c5846f1ed1df4a6
aes3xs <aes3xs@gmail.com>
2017-03-03 02:17:49 +0200

    Fix LazyHeapProxy test

--- tests/Heap/LazyHeapProxyTest.php
+++ tests/Heap/LazyHeapProxyTest.php
@@ -11,14 +11,15 @@
 
 namespace Aes3xs\Yodler\Tests\Heap;
 
-use Aes3xs\Yodler\Deployer\DeployContextInterface;
+use Aes3xs\Yodler\Connection\ConnectionInterface;
+use Aes3xs\Yodler\Console\Application;
+use Aes3xs\Yodler\Event\ConsoleRunEvent;
 use Aes3xs\Yodler\Event\DeployEvent;
 use Aes3xs\Yodler\Exception\RuntimeException;
 use Aes3xs\Yodler\Heap\HeapFactoryInterface;
 use Aes3xs\Yodler\Heap\HeapInterface;
 use Aes3xs\Yodler\Heap\LazyHeapProxy;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Aes3xs\Yodler\Scenario\ScenarioInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -30,16 +31,17 @@ class LazyHeapProxyTest extends \PHPUnit_Framework_TestCase
         $heapMock->method($method)->with($name)->willReturn($value);
         $inputMock = $this->createMock(InputInterface::class);
         $outputMock = $this->createMock(OutputInterface::class);
-        $commandMock = $this->createMock(Command::class);
-        $commandEvent = new ConsoleCommandEvent($commandMock, $inputMock, $outputMock);
-        $deployContext = $this->createMock(DeployContextInterface::class);
-        $deployEvent = new DeployEvent($deployContext);
+        $applicationMock = $this->createMock(Application::class);
+        $consoleRunEvent = new ConsoleRunEvent($applicationMock, $inputMock, $outputMock);
+        $scenarioMock = $this->createMock(ScenarioInterface::class);
+        $connectionMock = $this->createMock(ConnectionInterface::class);
+        $deployEvent = new DeployEvent($scenarioMock, $connectionMock);
         $heapFactory = $this->createMock(HeapFactoryInterface::class);
-        $heapFactory->method('create')->with($deployContext)->willReturn($heapMock);
+        $heapFactory->expects($this->at(0))->method('create')->with($scenarioMock, $connectionMock, $inputMock, $outputMock)->willReturn($heapMock);
 
         $heap = new LazyHeapProxy($heapFactory);
 
-        $heap->onCommand($commandEvent);
+        $heap->onConsoleRun($consoleRunEvent);
         $heap->onDeploy($deployEvent);
 
         $this->assertSame($value, $heap->{$method}($name));
@@ -55,11 +57,6 @@ class LazyHeapProxyTest extends \PHPUnit_Framework_TestCase
         $this->assertLazyHeapProxyMethod('get', 'test', 'value');
     }
 
-    public function testResolve()
-    {
-        $this->assertLazyHeapProxyMethod('resolve', 'test', 'value');
-    }
-
     public function testResolveString()
     {
         $this->assertLazyHeapProxyMethod('resolveString', 'test', 'value');
@@ -68,11 +65,6 @@ class LazyHeapProxyTest extends \PHPUnit_Framework_TestCase
     public function testResolveExpression()
     {
         $this->assertLazyHeapProxyMethod('resolveExpression', 'test', 'value');
-    }
-
-    public function testGetDependencies()
-    {
-        $this->assertLazyHeapProxyMethod('getDependencies', 'test', ['value']);
     }
 
     public function testInitializeException()
