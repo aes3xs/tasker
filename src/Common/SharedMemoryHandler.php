1d34d5c4839b2c4919ad1b52dcbbc6218903f743
aes3xs <aes3xs@gmail.com>
2017-07-07 15:23:35 +0200

    Rewrite shared memory implementation with temporary file instead of system shared memory

--- src/Common/SharedMemoryHandler.php
+++ src/Common/SharedMemoryHandler.php
@@ -11,54 +11,30 @@
 
 namespace Aes3xs\Yodler\Common;
 
-use Aes3xs\Yodler\Exception\RuntimeException;
-
 /**
  * Implements shared memory feature to share data between deploy processes.
  *
- * Wraps shared memory functionality from PHP core.
  * Stores data in JSON format.
+ * Uses temporary file.
  */
 class SharedMemoryHandler
 {
-    const MODE_READ = 'a';
-    const MODE_CREATE = 'c';
-    const MODE_READ_WRITE = 'w';
-    const MODE_NEW = 'n';
-
-    const PERMISSIONS = 0666;
-
     /**
-     * @var int
+     * @var string
      */
-    protected $key;
+    protected $file;
 
     /**
      * Constructor.
+     *
      * @param string $name
      */
     public function __construct($name)
     {
-        // Shared memory can be configured to read by all users
-        // But it cannot be deleted (for resizing) by anyone except root and owner
-        // So we'll force shared memory create distinct ids by same key for different users
-        $this->key = $this->getIntegerHash(posix_getuid() . $name);
-    }
+        $this->file = sys_get_temp_dir() . '/_yodler_' . $name;
 
-    /**
-     * Generate integer hash from string.
-     *
-     * Integer key is required by PHP shared memory functions.
-     *
-     * http://stackoverflow.com/a/6315489
-     *
-     * @param string $str
-     * @return int
-     */
-    protected function getIntegerHash($str)
-    {
-        $u = unpack('N2', sha1($str, true));
-        return ($u[1] << 32) | $u[2];
+        touch($this->file);
+        chmod($this->file, 0777);
     }
 
     /**
@@ -70,24 +46,20 @@ class SharedMemoryHandler
      */
     public function read()
     {
-        $shmid = @shmop_open($this->key, self::MODE_READ, self::PERMISSIONS, 0);
-
-        if (!$shmid) {
+        if (!file_exists($this->file)) {
             return null;
         }
 
-        $data = null;
-        $size = shmop_size($shmid);
+        $content = file_get_contents($this->file);
 
-        if ($size) {
-            $content = shmop_read($shmid, 0, $size);
-            $data = json_decode($content, true);
-            if (JSON_ERROR_NONE !== json_last_error()) {
-                throw new \InvalidArgumentException('json_decode error: ' . json_last_error_msg());
-            }
+        if (!$content) {
+            return null;
         }
 
-        shmop_close($shmid);
+        $data = json_decode($content, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new \InvalidArgumentException('json_decode error: ' . json_last_error_msg());
+        }
 
         return $data;
     }
@@ -99,36 +71,18 @@ class SharedMemoryHandler
      */
     public function write($data)
     {
-        $shmid = @shmop_open($this->key, self::MODE_READ, self::PERMISSIONS, 0);
-
-        if ($shmid) {
-            shmop_delete($shmid);
-            shmop_close($shmid);
-        }
-
         $content = json_encode($data);
         if (JSON_ERROR_NONE !== json_last_error()) {
             throw new \InvalidArgumentException('json_encode error: ' . json_last_error_msg());
         }
-        $size = mb_strlen($content, 'UTF-8');
 
-        $shmid = @shmop_open($this->key, self::MODE_CREATE, self::PERMISSIONS, $size);
-
-        if (!$shmid) {
-            throw new RuntimeException('Shared memory write error: ' . error_get_last()['message']);
-        }
-
-        shmop_write($shmid, $content, 0);
-        shmop_close($shmid);
+        file_put_contents($this->file, $content);
     }
 
     public function delete()
     {
-        $shmid = @shmop_open($this->key, self::MODE_READ, self::PERMISSIONS, 0);
-
-        if ($shmid) {
-            shmop_delete($shmid);
-            shmop_close($shmid);
+        if (file_exists($this->file)) {
+            unlink($this->file);
         }
     }
 }
